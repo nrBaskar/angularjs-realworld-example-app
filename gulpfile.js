90f3582f50817763fce305a0d7b64067d2f9ed61
nrBaskar <106078300+nrBaskar@users.noreply.github.com>
2022-11-08 14:26:19 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -73,10 +73,10 @@ gulp.task('default', ['html', 'browserify'], function() {
 
   browserSync.init(['./build/**/**.**'], {
     server: "./build",
-    port: 4000,
+    port: 8081,
     notify: false,
     ui: {
-      port: 4001
+      port: 8082
     }
   });
 
