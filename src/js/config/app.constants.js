5e7d0267a5c551a55038385591f3abefc2e2ba04
nrBaskar <106078300+nrBaskar@users.noreply.github.com>
2023-02-13 15:18:46 +0530

    Update app.constants.js

--- src/js/config/app.constants.js
+++ src/js/config/app.constants.js
@@ -1,6 +1,6 @@
 const AppConstants = {
   // api: 'https://conduit.productionready.io/api',
-  api: 'http://44.201.163.196:3000/api',
+  api: 'http://44.201.163.196:8081/api',
   jwtKey: 'jwtToken',
   appName: 'Conduit',
 };
